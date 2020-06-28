3ddb76764e9631ec6772ff87182f74193ae590a3
carlangastejada <cgrt17062011@gmail.com>
2020-06-28 19:30:11 -0400

    yujuu

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,12 @@ const menuclose = document.getElementById('menu-close-button')
 const menu = document.getElementById('menu')
 const clickeditem = document.getElementsByClassName('menu-links-list__link')
 const gotop = document.getElementById('go-top')
+const logo = document.getElementById('logo')
 
 menubtn.addEventListener('click', () => menu.style.visibility = 'visible')
 menuclose.addEventListener('click', () => menu.style.visibility = 'hidden')
 gotop.addEventListener('click', () => window.scrollTo(0, 0))
+logo.addEventListener('click', () => window.scrollTo(0, 0))
 
 for (item of clickeditem) {
     item.addEventListener('click', () => menu.style.visibility = 'hidden')
