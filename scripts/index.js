a711f42be4eb0efd754f710a5af86bb1a9efbf2a
carlangastejada <cgrt17062011@gmail.com>
2020-06-28 19:25:33 -0400

    added a menu with js

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+const menubtn = document.getElementById('menu-open-button')
+const menuclose = document.getElementById('menu-close-button')
+const menu = document.getElementById('menu')
+const clickeditem = document.getElementsByClassName('menu-links-list__link')
+const gotop = document.getElementById('go-top')
+
+menubtn.addEventListener('click', () => menu.style.visibility = 'visible')
+menuclose.addEventListener('click', () => menu.style.visibility = 'hidden')
+gotop.addEventListener('click', () => window.scrollTo(0, 0))
+
+for (item of clickeditem) {
+    item.addEventListener('click', () => menu.style.visibility = 'hidden')
+}
