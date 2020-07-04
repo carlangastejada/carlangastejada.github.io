0075d42ae467066bda9e37c60a63aaad02406f93
carlangastejada <cgrt17062011@gmail.com>
2020-07-04 14:25:14 -0400

    aaa

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 const logo = document.getElementById('logo')
-const menubtn = document.getElementById('menu-open-button')
-const menuclose = document.getElementById('menu-close-button')
+const menu_open_btn = document.getElementById('menu-open-button')
+const menu_close_btn = document.getElementById('menu-close-button')
 const menu = document.getElementById('menu')
-const links = document.getElementsByClassName('links-list__link')
+const navbar_links = document.getElementsByClassName('links-list__link')
 
 
 const showMenu = () => menu.style.visibility = 'visible'
@@ -10,13 +10,13 @@ const closeMenu = () => menu.style.visibility = 'hidden'
 const enableMenu = () => {
     if (window.outerWidth < 768) {
         menu.style.visibility = "hidden"
-        menubtn.addEventListener('click', showMenu)
-        menuclose.addEventListener('click', closeMenu)
-        for (item of links) item.addEventListener('click', closeMenu)
+        menu_open_btn.addEventListener('click', showMenu)
+        menu_close_btn.addEventListener('click', closeMenu)
+        for (item of navbar_links) item.addEventListener('click', closeMenu)
     } else {
-        menubtn.removeEventListener('click', showMenu)
-        menuclose.removeEventListener('click', closeMenu)
-        for (item of links) item.removeEventListener('click', closeMenu)
+        menu_open_btn.removeEventListener('click', showMenu)
+        menu_close_btn.removeEventListener('click', closeMenu)
+        for (item of navbar_links) item.removeEventListener('click', closeMenu)
         menu.style.visibility = "visible"
     }
 }
