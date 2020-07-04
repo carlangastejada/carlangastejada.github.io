b8fe2302fa8905b05fb681549c6557734da7cc43
carlangastejada <cgrt17062011@gmail.com>
2020-07-04 17:15:13 -0400

    added splash screen

---
+++ scripts/splash-screen.js
@@ -0,0 +1,9 @@
+const splash_screen = document.getElementById('splash-screen')
+
+window.addEventListener('load', () => {
+    splash_screen.style.opacity = '0'
+})
+
+splash_screen.addEventListener('transitionend', () => {
+    splash_screen.style.display = 'none'
+})
