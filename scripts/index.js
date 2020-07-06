2ccb01ca70e4dd514e211bfdb791bc68a354529d
carlangastejada <cgrt17062011@gmail.com>
2020-07-06 01:44:24 -0400

    aaa

--- scripts/index.js
+++ scripts/index.js
@@ -12,10 +12,12 @@ const closeMenu = () => menu.style.transform = 'translateX(100%)'
 const enableMenu = () => {
     if (window.outerWidth < 768) {
         menu.style.transform = 'translateX(100%)'
+        menu.addEventListener('click', closeMenu)
         menu_open_btn.addEventListener('click', showMenu)
         menu_close_btn.addEventListener('click', closeMenu)
         for (item of menu_links) item.addEventListener('click', closeMenu)
     } else {
+        menu.removeEventListener('click', closeMenu)
         menu_open_btn.removeEventListener('click', showMenu)
         menu_close_btn.removeEventListener('click', closeMenu)
         for (item of menu_links) item.removeEventListener('click', closeMenu)
