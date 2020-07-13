9d8d63c4ca5c9b8adca8938c919423bdb9fca617
carlangastejada <cgrt17062011@gmail.com>
2020-07-13 19:50:06 -0400

    Code formatted using prettier

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,25 @@
-const menu_open_btn = document.getElementById('header__menu-button')
-const menu_close_btn = document.getElementById('menu__close-button')
-const menu = document.getElementById('menu')
-const menu_links = document.getElementsByClassName('menu__link')
+const menu_open_btn = document.getElementById("header__menu-button");
+const menu_close_btn = document.getElementById("menu__close-button");
+const menu = document.getElementById("menu");
+const menu_links = document.getElementsByClassName("menu__link");
 
-const showMenu = () => menu.style.transform = 'translateX(0)'
-const closeMenu = () => menu.style.transform = 'translateX(100%)'
+const showMenu = () => (menu.style.transform = "translateX(0)");
+const closeMenu = () => (menu.style.transform = "translateX(100%)");
 const enableMenu = () => {
-    if (window.outerWidth < 768) {
-        menu.style.transform = 'translateX(100%)'
-        menu.addEventListener('click', closeMenu)
-        menu_open_btn.addEventListener('click', showMenu)
-        menu_close_btn.addEventListener('click', closeMenu)
-        for (item of menu_links) item.addEventListener('click', closeMenu)
-    } else {
-        menu.removeEventListener('click', closeMenu)
-        menu_open_btn.removeEventListener('click', showMenu)
-        menu_close_btn.removeEventListener('click', closeMenu)
-        for (item of menu_links) item.removeEventListener('click', closeMenu)
-        menu.style.transform = 'translateX(0)'
-    }
-}
+  if (window.outerWidth < 768) {
+    menu.style.transform = "translateX(100%)";
+    menu.addEventListener("click", closeMenu);
+    menu_open_btn.addEventListener("click", showMenu);
+    menu_close_btn.addEventListener("click", closeMenu);
+    for (item of menu_links) item.addEventListener("click", closeMenu);
+  } else {
+    menu.removeEventListener("click", closeMenu);
+    menu_open_btn.removeEventListener("click", showMenu);
+    menu_close_btn.removeEventListener("click", closeMenu);
+    for (item of menu_links) item.removeEventListener("click", closeMenu);
+    menu.style.transform = "translateX(0)";
+  }
+};
 
-window.addEventListener('load', enableMenu)
-window.addEventListener('resize', enableMenu)
+window.addEventListener("load", enableMenu);
+window.addEventListener("resize", enableMenu);
--- scripts/splash-screen.js
+++ scripts/splash-screen.js
@@ -1,18 +1,21 @@
-const head = document.getElementById('head')
+const head = document.getElementById("head");
 
-head.insertAdjacentHTML('afterend', `
+head.insertAdjacentHTML(
+  "afterend",
+  `
 <div class="splash-screen" id="splash-screen">
     <img src="./img/favicon.svg" class="splash-screen__logo">
     <div class="splash-screen__loader"></div>
 </div>
-`)
+`
+);
 
-const splash_screen = document.getElementById('splash-screen')
+const splash_screen = document.getElementById("splash-screen");
 
-window.addEventListener('load', () => {
-    splash_screen.style.opacity = '0'
-})
+window.addEventListener("load", () => {
+  splash_screen.style.opacity = "0";
+});
 
-splash_screen.addEventListener('transitionend', () => {
-    splash_screen.style.display = 'none'
-})
+splash_screen.addEventListener("transitionend", () => {
+  splash_screen.style.display = "none";
+});
