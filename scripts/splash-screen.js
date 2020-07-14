106cbc5fabe804240d4cfe3eee025cf6ac4a946a
carlangastejada <cgrt17062011@gmail.com>
2020-07-14 02:31:52 -0400

    aaaa

--- scripts/splash-screen.js
+++ scripts/splash-screen.js
@@ -1,3 +1,15 @@
+const body = document.getElementById("body");
+
+body.insertAdjacentHTML(
+  "afterbegin",
+  `
+<div class="splash-screen" id="splash-screen">
+      <img src="./img/favicon.svg" class="splash-screen__logo">
+      <div class="splash-screen__loader"></div>
+    </div>
+`
+);
+
 const splash_screen = document.getElementById("splash-screen");
 
 window.addEventListener("load", () => {
