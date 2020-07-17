734855fa6af24166ec4098c73ae3434fb3ad566b
carlangastejada <cgrt17062011@gmail.com>
2020-07-17 16:56:33 -0400

    enhanced slogan animation

--- scripts/modules/welcome-slogan.js
+++ scripts/modules/welcome-slogan.js
@@ -1,13 +1,12 @@
 const slogan = document.getElementById("welcome__slogan");
 
 window.setInterval(() => {
-  if (slogan.innerHTML == "¿Necesitas a un diseñador web?") {
-    slogan.style.opacity = "0";
-    slogan.addEventListener("transitionend", () => {
-      slogan.innerHTML = `Diseña tu presencia web conmigo`;
-      slogan.style.opacity = "1";
-    });
-  } else {
+  slogan.style.opacity = "0";
+  slogan.addEventListener("transitionend", () => {
+    slogan.innerHTML = `Diseña tu presencia web conmigo`;
+    slogan.style.opacity = "1";
+  });
+  if (slogan.innerHTML == "Diseña tu presencia web conmigo") {
     slogan.style.opacity = "0";
     slogan.addEventListener("transitionend", () => {
       slogan.innerHTML = `¿Necesitas a un diseñador web?`;
