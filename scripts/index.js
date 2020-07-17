e89ac52496e8525814b452586c6474d01530b229
carlangastejada <cgrt17062011@gmail.com>
2020-07-17 02:05:56 -0400

    modularized js

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,11 @@
-const menu_open_btn = document.getElementById("header__menu-button");
-const menu_close_btn = document.getElementById("menu__close-button");
-const menu = document.getElementById("menu");
-const menu_links = document.getElementsByClassName("menu__link");
+import * as splashscreen from "./modules/splash-screen.js";
+import NavMenu from "./modules/menu.js";
 
-const showMenu = () => (menu.style.transform = "translateX(0)");
-const closeMenu = () => (menu.style.transform = "translateX(100%)");
-const enableMenu = () => {
-  if (window.outerWidth < 768) {
-    menu.style.transform = "translateX(100%)";
-    menu.addEventListener("click", closeMenu);
-    menu_open_btn.addEventListener("click", showMenu);
-    menu_close_btn.addEventListener("click", closeMenu);
-    for (item of menu_links) item.addEventListener("click", closeMenu);
-  } else {
-    menu.removeEventListener("click", closeMenu);
-    menu_open_btn.removeEventListener("click", showMenu);
-    menu_close_btn.removeEventListener("click", closeMenu);
-    for (item of menu_links) item.removeEventListener("click", closeMenu);
-    menu.style.transform = "translateX(0)";
-  }
-};
+splashscreen.injectHTML();
+const navbar_menu = new NavMenu("menu");
 
-window.addEventListener("load", enableMenu);
-window.addEventListener("resize", enableMenu);
+window.addEventListener("load", splashscreen.quit);
+window.addEventListener("load", navbar_menu.enable);
+window.addEventListener("resize", navbar_menu.enable);
+
+import "./modules/testimonial-card.js";
