a48a9e63b69bf485f329e2914bb8cbfbb65b24c7
carlangastejada <cgrt17062011@gmail.com>
2020-07-21 01:40:39 -0400

    enabled portfolio

--- scripts/modules/welcome-slogan.js
+++ scripts/modules/welcome-slogan.js
@@ -1,16 +1,18 @@
 const slogan = document.getElementById("welcome__slogan");
 
-window.setInterval(() => {
-  slogan.style.opacity = "0";
-  slogan.addEventListener("transitionend", () => {
-    slogan.innerHTML = `Diseña tu presencia web conmigo`;
-    slogan.style.opacity = "1";
-  });
-  if (slogan.innerHTML == "Diseña tu presencia web conmigo") {
+if (slogan) {
+  window.setInterval(() => {
     slogan.style.opacity = "0";
     slogan.addEventListener("transitionend", () => {
-      slogan.innerHTML = `¿Necesitas a un diseñador web?`;
+      slogan.innerHTML = `Diseña tu presencia web conmigo`;
       slogan.style.opacity = "1";
     });
-  }
-}, 3000);
+    if (slogan.innerHTML == "Diseña tu presencia web conmigo") {
+      slogan.style.opacity = "0";
+      slogan.addEventListener("transitionend", () => {
+        slogan.innerHTML = `¿Necesitas a un diseñador web?`;
+        slogan.style.opacity = "1";
+      });
+    }
+  }, 3000);
+}
